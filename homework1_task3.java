dce1d4e973d9679cae7d38ffbca13daf665ca5d5
oleg voronin <oleg83voronin@gmail.com>
2024-09-21 16:11:03 +0300

    add new fil and three task

---
+++ homework1_task3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class homework1_task3 {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        
+                System.out.println("Please enter your namber: ");
+        
+                int number = sc.nextInt();
+                System.out.println(sumDigits(number));
+    }
+    
+    public static int sumDigits(int number){
+        int sum = 0;
+        
+        while(number > 0){
+            sum += number % 10;
+            number /= 10;
+        }
+        
+        return sum;
+    }
+}
