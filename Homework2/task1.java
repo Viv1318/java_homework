e08934fafcbfc834f605098262a20567ad1caa49
oleg voronin <oleg83voronin@gmail.com>
2024-09-27 14:07:21 +0300

    modified task1

--- Homework2/task1.java
+++ Homework2/task1.java
@@ -1,5 +1,47 @@
+// Дана строка базового URL:
+// https://example.com/search?
+// Сформируйте полный URL, добавив к нему параметры для поиска. Параметры
+// передаются в виде строки, где ключи и значения разделены =, а пары
+// ключ-значение разделены &. Если значение null, то параметр не должен
+// попадать в URL.
+// Пример:
+// params = "query=java&sort=desc&filter=null"
+// Результат:
+// https://example.com/search?query=java&sort=desc
+
+
 package Homework2;
 
 public class task1 {
+
+    public static void main(String[] args) {
+        String baseUrl = "https://example.com/search?";
+        String params = "query=java&sort=desc&filter=null";
+        
+        System.out.println(createUrlWithParams(baseUrl, params));
+       
+ 
+    }
     
+    public static String createUrlWithParams(String baseUrl, String params) {
+        String[] pairs = params.split("&");
+        StringBuilder urlBuilder = new StringBuilder(baseUrl);
+
+        for ( int i = 0; i < pairs.length; i++ ) {
+            
+            String[] keyValue = pairs[i].split("=");
+            if (!"null".equals(keyValue[1])) {
+                if (i > 0){
+                    urlBuilder.append("&");
+                }
+    
+                urlBuilder.append(keyValue[0]).append("=").append(keyValue[1]);
+            }
+         
+        }
+        return urlBuilder.toString();
+        
+
+
+    }
 }
---
+++ Homework2/task2.java
@@ -0,0 +1,44 @@
+// Задача 2. Создание CSV-строки из массива объектов
+// Дан массив объектов, где каждый объект представляет собой строку данных, и
+// массив заголовков. Создайте строку CSV, где строки данных разделяются новой
+// строкой, а значения в строках разделяются запятыми.
+// Пример:
+// String[] headers = {"Name", "Age", "City"};
+// String[][] data = {
+// {"John", "30", "New York"},
+// {"Alice", "25", "Los Angeles"},
+// {"Bob", "35", "Chicago"}
+// };
+// Результат:
+// Name,Age,City
+// John,30,New York
+// Alice,25,Los Angeles
+// Bob,35,Chicago
+
+
+
+package Homework2;
+
+public class task2 {
+    
+    public static void main(String[] args) {
+        String[] headers = {"Name", "Age", "City"};
+        String[][] data = {{"John", "30", "New York"},{"Alice", "25", "Los Angeles"},{"Bob", "35", "Chicago"}};
+        
+        System.out.println(createCSVString(headers, data));
+    }
+    
+    public static String createCSVString(String[] headers, String[][] data) {
+        StringBuilder sb = new StringBuilder();
+        
+        // Записываем заголовки
+        sb.append(String.join(",", headers)).append("\n");
+        
+        // Записываем данные
+        for (String[] row : data) {
+            sb.append(String.join(",", row)).append("\n");
+        }
+        
+        return sb.toString();
+    }
+}
