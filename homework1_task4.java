044f985586b3445305dad2d09fea0d087f1f80e2
oleg voronin <oleg83voronin@gmail.com>
2024-09-27 09:42:12 +0300

    modified file

--- homework1_task4.java
+++ homework1_task4.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class homework1_task4 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number a: ");
-        int a = scanner.nextInt();
-        System.out.println("Enter number b: ");
-        int b = scanner.nextInt();
-        System.out.println("Enter number c: ");
-        int c = scanner.nextInt();
-        System.out.println("Maximum of " + a + " and " + b + " is: " + findMaxofTwo(a, b));
-        System.out.println("Maximum of " + a + " , " + b + " and " + c + " is: " + findMaxFhree(a, b, c));
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter number a: ");
+            int a = scanner.nextInt();
+            System.out.println("Enter number b: ");
+            int b = scanner.nextInt();
+            System.out.println("Enter number c: ");
+            int c = scanner.nextInt();
+            System.out.println("Maximum of " + a + " and " + b + " is: " + findMaxofTwo(a, b));
+            System.out.println("Maximum of " + a + " , " + b + " and " + c + " is: " + findMaxFhree(a, b, c));
+        }
     }
 
     public static int findMaxofTwo(int a, int b) {
