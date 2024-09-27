c012e12256e0568e9c749ac28d6ef8c576ede6ee
oleg voronin <oleg83voronin@gmail.com>
2024-09-27 09:46:51 +0300

    modified file

--- homework1_task3.java
+++ homework1_task3.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class homework1_task3 {
 
     public static void main(String[] args) {
-         try(Scanner sc = new Scanner(System.in));
-        
-                System.out.println("Please enter your namber: ");
-        
-                int number = sc.nextInt();
-                System.out.println(sumDigits(number));
+         try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Please enter your namber: ");
+      
+            int number = sc.nextInt();
+            System.out.println(sumDigits(number));
+        }
     }
     
     public static int sumDigits(int number){
